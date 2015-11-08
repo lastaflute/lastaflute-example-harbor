d9bb53058b512c9cd500754184e9301bd6117063
jflute <dbflute@gmail.com>
2015-11-08 23:53:39 +0900

    migrating thymeleaf

--- src/main/java/org/docksidestage/app/web/product/ProductSearchForm.java
+++ src/main/java/org/docksidestage/app/web/product/ProductSearchForm.java
@@ -16,13 +16,18 @@
 package org.docksidestage.app.web.product;
 
 import org.docksidestage.dbflute.allcommon.CDef;
+import org.hibernate.validator.constraints.Length;
 
 /**
  * @author jflute
  */
 public class ProductSearchForm {
 
+    @Length(max = 10)
     public String productName;
+
     public CDef.ProductStatus productStatus;
+
+    @Length(max = 5)
     public String purchaseMemberName;
 }

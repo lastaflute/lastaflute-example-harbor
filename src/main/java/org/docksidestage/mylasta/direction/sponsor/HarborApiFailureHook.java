0c3f0d552c64d3f5a9811a8303ce05c428736eb1
jflute <dbflute@gmail.com>
2015-12-18 16:52:15 +0900

    refactor api failure hook

--- src/main/java/org/docksidestage/mylasta/direction/sponsor/HarborApiFailureHook.java
+++ src/main/java/org/docksidestage/mylasta/direction/sponsor/HarborApiFailureHook.java
@@ -15,14 +15,18 @@
  */
 package org.docksidestage.mylasta.direction.sponsor;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.dbflute.optional.OptionalThing;
 import org.lastaflute.web.api.ApiFailureHook;
 import org.lastaflute.web.api.ApiFailureResource;
-import org.lastaflute.web.login.exception.LoginUnauthorizedException;
+import org.lastaflute.web.login.exception.LoginFailureException;
+import org.lastaflute.web.login.exception.LoginRequiredException;
 import org.lastaflute.web.response.ApiResponse;
 import org.lastaflute.web.response.JsonResponse;
 
@@ -31,29 +35,66 @@ import org.lastaflute.web.response.JsonResponse;
  */
 public class HarborApiFailureHook implements ApiFailureHook { // #change_it for handling API failure
 
+    // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+    // [Front-side Implementation Image]
+    //
+    // if (HTTP Status: 200) { // success
+    //     JsonBean bean = parseJsonAsSuccess(response);
+    //     ...(do process per action)
+    // } else if (HTTP Status: 400 to 499) { // validation error, application exception, client exception
+    //     try {
+    //         FailureBean bean = parseJsonAsFailure(response);
+    //         ...(show bean.messageList or do process per bean.failureType)
+    //     } catch (ParseFailureException e) { // e.g. 404 from web server, web container
+    //         showSystemError(e);
+    //     }
+    // } else { // basically 500, server exception
+    //     showSystemError();
+    // }
+    // _/_/_/_/_/_/_/_/_/_/
+
     // ===================================================================================
     //                                                                          Definition
     //                                                                          ==========
-    protected static final int HTTP_BAD_REQUEST = HttpServletResponse.SC_BAD_REQUEST;
-    protected static final int HTTP_UNAUTHORIZED = HttpServletResponse.SC_UNAUTHORIZED;
+    protected static final int BUSINESS_FAILURE_STATUS = HttpServletResponse.SC_BAD_REQUEST;
+    protected static final Map<Class<?>, TooSimpleFailureType> failureTypeMap; // for application exception
+
+    static { // you can add mapping of failure type with exception
+        final Map<Class<?>, TooSimpleFailureType> makingMap = new HashMap<Class<?>, TooSimpleFailureType>();
+        makingMap.put(LoginFailureException.class, TooSimpleFailureType.LOGIN_FAILURE);
+        makingMap.put(LoginRequiredException.class, TooSimpleFailureType.LOGIN_REQUIRED);
+        failureTypeMap = Collections.unmodifiableMap(makingMap);
+    }
 
     // ===================================================================================
     //                                                                    Business Failure
     //                                                                    ================
     @Override
     public ApiResponse handleValidationError(ApiFailureResource resource) {
-        final TooSimpleFailureBean bean = createFailureBean(TooSimpleBizStatus.VALIDATION_ERROR, resource);
-        return asJson(bean).httpStatus(HTTP_BAD_REQUEST);
+        final TooSimpleFailureBean bean = createFailureBean(TooSimpleFailureType.VALIDATION_ERROR, resource);
+        return asJson(bean).httpStatus(BUSINESS_FAILURE_STATUS);
     }
 
     @Override
     public ApiResponse handleApplicationException(ApiFailureResource resource, RuntimeException cause) {
-        final TooSimpleFailureBean bean = createFailureBean(TooSimpleBizStatus.APPLICATION_EXCEPTION, resource);
-        return asJson(bean).httpStatus(chooseApplicationExceptionHttpStatus(cause));
+        final TooSimpleFailureType failureType = findAssignableValue(failureTypeMap, cause.getClass()).orElseGet(() -> {
+            return TooSimpleFailureType.APPLICATION_EXCEPTION;
+        });
+        final TooSimpleFailureBean bean = createFailureBean(failureType, resource);
+        return asJson(bean).httpStatus(BUSINESS_FAILURE_STATUS);
     }
 
-    protected int chooseApplicationExceptionHttpStatus(RuntimeException cause) {
-        return cause instanceof LoginUnauthorizedException ? HTTP_UNAUTHORIZED : HTTP_BAD_REQUEST;
+    protected <VALUE> OptionalThing<VALUE> findAssignableValue(Map<Class<?>, VALUE> map, Class<?> key) {
+        final VALUE found = map.get(key);
+        if (found != null) {
+            return OptionalThing.of(found);
+        } else {
+            return OptionalThing.migratedFrom(map.entrySet().stream().filter(entry -> {
+                return entry.getKey().isAssignableFrom(key);
+            }).map(entry -> entry.getValue()).findFirst(), () -> {
+                throw new IllegalStateException("Not found the exception type in the map: " + map.keySet());
+            });
+        }
     }
 
     // ===================================================================================
@@ -61,7 +102,7 @@ public class HarborApiFailureHook implements ApiFailureHook { // #change_it for 
     //                                                                      ==============
     @Override
     public OptionalThing<ApiResponse> handleClientException(ApiFailureResource resource, RuntimeException cause) {
-        final TooSimpleFailureBean bean = createFailureBean(TooSimpleBizStatus.CLIENT_EXCEPTION, resource);
+        final TooSimpleFailureBean bean = createFailureBean(TooSimpleFailureType.CLIENT_EXCEPTION, resource);
         return OptionalThing.of(asJson(bean)); // HTTP status will be automatically sent as client error for the cause
     }
 
@@ -77,23 +118,26 @@ public class HarborApiFailureHook implements ApiFailureHook { // #change_it for 
         return new JsonResponse<TooSimpleFailureBean>(bean);
     }
 
-    protected TooSimpleFailureBean createFailureBean(TooSimpleBizStatus bizStatus, ApiFailureResource resource) {
-        return new TooSimpleFailureBean(bizStatus, resource.getMessageList());
-    }
-
-    public static enum TooSimpleBizStatus {
-        VALIDATION_ERROR, APPLICATION_EXCEPTION, CLIENT_EXCEPTION
+    protected TooSimpleFailureBean createFailureBean(TooSimpleFailureType failureType, ApiFailureResource resource) {
+        return new TooSimpleFailureBean(failureType, resource.getMessageList());
     }
 
     public static class TooSimpleFailureBean {
 
         public final String notice = "[Attension] tentative JSON so you should change it: " + HarborApiFailureHook.class;
-        public final TooSimpleBizStatus bizStatus;
+        public final TooSimpleFailureType failureType;
         public final List<String> messageList;
 
-        public TooSimpleFailureBean(TooSimpleBizStatus bizStatus, List<String> messageList) {
-            this.bizStatus = bizStatus;
+        public TooSimpleFailureBean(TooSimpleFailureType failureType, List<String> messageList) {
+            this.failureType = failureType;
             this.messageList = messageList;
         }
+    }
+
+    public static enum TooSimpleFailureType {
+        VALIDATION_ERROR // special type
+        , LOGIN_FAILURE, LOGIN_REQUIRED // specific type of application exception
+        , APPLICATION_EXCEPTION // default type of application exception
+        , CLIENT_EXCEPTION
     }
 }

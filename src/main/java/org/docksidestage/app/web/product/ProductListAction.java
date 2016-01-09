75def846f6fb565d6596f46e56619772813d00ff
jflute <dbflute@gmail.com>
2016-01-09 20:46:02 +0900

    fix copyright, real begin year is 2015 and now 2016

--- src/main/java/org/docksidestage/app/web/product/ProductListAction.java
+++ src/main/java/org/docksidestage/app/web/product/ProductListAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2015 the original author or authors.
+ * Copyright 2015-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ import org.docksidestage.dbflute.exentity.Product;
 import org.lastaflute.web.Execute;
 import org.lastaflute.web.login.AllowAnyoneAccess;
 import org.lastaflute.web.response.HtmlResponse;
+import org.lastaflute.web.response.JsonResponse;
 
 /**
  * @author jflute
@@ -56,6 +57,11 @@ public class ProductListAction extends HarborBaseAction {
             data.register("beans", beans);
             registerPagingNavi(data, page, form);
         });
+    }
+
+    @Execute
+    public JsonResponse<Object> sea() {
+        return asJson(new Object());
     }
 
     // ===================================================================================

1a906c04897b19f8c4d55a95bfa17476f752630b
jflute <dbflute@gmail.com>
2016-09-04 11:56:56 +0900

    remove web dependency

--- src/main/java/org/docksidestage/app/web/base/HarborBaseAction.java
+++ src/main/java/org/docksidestage/app/web/base/HarborBaseAction.java
@@ -18,7 +18,7 @@ package org.docksidestage.app.web.base;
 import javax.annotation.Resource;
 
 import org.dbflute.optional.OptionalThing;
-import org.docksidestage.app.web.base.context.AccessContextAssist;
+import org.docksidestage.app.logic.context.AccessContextLogic;
 import org.docksidestage.app.web.base.login.HarborLoginAssist;
 import org.docksidestage.app.web.base.view.HeaderBean;
 import org.docksidestage.mylasta.action.HarborHtmlPath;
@@ -54,7 +54,7 @@ public abstract class HarborBaseAction extends TypicalAction // has several inte
     @Resource
     private DoubleSubmitManager doubleSubmitManager;
     @Resource
-    private AccessContextAssist accessContextAssist;
+    private AccessContextLogic accessContextLogic;
     @Resource
     private HarborLoginAssist loginAssist;
 
@@ -99,7 +99,9 @@ public abstract class HarborBaseAction extends TypicalAction // has several inte
     @Override
     protected AccessContextArranger newAccessContextArranger() { // for framework
         return resource -> {
-            return accessContextAssist.create(resource, () -> myUserType(), () -> getUserBean(), () -> myAppType());
+            return accessContextLogic.create(resource, () -> myUserType(), () -> getUserBean().map(userBean -> {
+                return userBean.getUserId(); // as user expression
+            }), () -> myAppType());
         };
     }
 

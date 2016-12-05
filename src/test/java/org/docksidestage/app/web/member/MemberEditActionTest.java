7e85f2a3fc36b7e09cc9cbe1bd31140b4a678613
jflute <dbflute@gmail.com>
2016-12-05 20:27:43 +0900

    test double submit

--- src/test/java/org/docksidestage/app/web/member/MemberEditActionTest.java
+++ src/test/java/org/docksidestage/app/web/member/MemberEditActionTest.java
@@ -11,8 +11,7 @@ import org.docksidestage.dbflute.exentity.Member;
 import org.docksidestage.mylasta.action.HarborHtmlPath;
 import org.docksidestage.unit.UnitHarborTestCase;
 import org.lastaflute.web.response.HtmlResponse;
-import org.lastaflute.web.token.DoubleSubmitManager;
-import org.lastaflute.web.token.TokenErrorHook;
+import org.lastaflute.web.token.exception.DoubleSubmittedRequestException;
 
 /**
  * @author jflute
@@ -21,9 +20,10 @@ public class MemberEditActionTest extends UnitHarborTestCase {
 
     @Resource
     private MemberBhv memberBhv;
-    @Resource
-    private DoubleSubmitManager doubleSubmitManager;
 
+    // ===================================================================================
+    //                                                                             index()
+    //                                                                             =======
     public void test_index_success() {
         // ## Arrange ##
         MemberEditAction action = new MemberEditAction();
@@ -39,36 +39,52 @@ public class MemberEditActionTest extends UnitHarborTestCase {
         htmlData.assertHtmlForward(HarborHtmlPath.path_Member_MemberEditHtml);
         MemberEditForm form = htmlData.requiredPushedForm(MemberEditForm.class);
         assertEquals(member.getMemberName(), form.memberName);
-        assertTrue(doubleSubmitManager.isDoubleSubmittedRequest());
+        assertTokenSaved(action.getClass());
     }
 
+    // ===================================================================================
+    //                                                                            update()
+    //                                                                            ========
     public void test_update_success() {
         // ## Arrange ##
-        MemberEditAction action = new MemberEditAction() {
-            @Override
-            protected void verifyToken(TokenErrorHook errorResponseLambda) {
-                markHere("called");
-            }
-        };
+        MemberEditAction action = new MemberEditAction();
         inject(action);
-        int memberId = 1;
-        Member member = memberBhv.selectByPK(memberId).get();
-
-        MemberEditForm form = new MemberEditForm();
-        form.memberId = memberId;
-        form.memberName = "seaName";
-        form.memberAccount = "seaAccount";
-        form.memberStatus = CDef.MemberStatus.Provisional;
-        form.birthdate = LocalDate.of(2016, 12, 2);
-        form.versionNo = member.getVersionNo();
-        form.previousStatus = member.getMemberStatusCodeAsMemberStatus();
+        mockTokenRequested(action.getClass());
+        MemberEditForm form = prepareEditForm();
 
         // ## Act ##
         HtmlResponse response = action.update(form);
 
         // ## Assert ##
         TestingHtmlData htmlData = validateHtmlData(response);
-        htmlData.assertRedirect(MemberEditAction.class);
-        assertMarked("called");
+        htmlData.assertRedirect(action.getClass());
+        assertTokenVerified();
+    }
+
+    public void test_update_doubleSubmitted() {
+        // ## Arrange ##
+        MemberEditAction action = new MemberEditAction();
+        inject(action);
+        mockTokenRequestedAsDoubleSubmit(action.getClass());
+        MemberEditForm form = prepareEditForm();
+
+        // ## Act ##
+        // ## Assert ##
+        assertException(DoubleSubmittedRequestException.class, () -> action.update(form));
+    }
+
+    private MemberEditForm prepareEditForm() {
+        int memberId = 1;
+        Member member = memberBhv.selectByPK(memberId).get();
+
+        MemberEditForm form = new MemberEditForm();
+        form.memberId = memberId;
+        form.memberName = "sea";
+        form.memberAccount = "land";
+        form.memberStatus = CDef.MemberStatus.Provisional;
+        form.birthdate = LocalDate.of(2016, 12, 2);
+        form.versionNo = member.getVersionNo();
+        form.previousStatus = member.getMemberStatusCodeAsMemberStatus();
+        return form;
     }
 }

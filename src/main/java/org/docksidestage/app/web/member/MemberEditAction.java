115f5399b01433b8bef9bac3edfd954306ae68e5
jflute <dbflute@gmail.com>
2016-12-15 21:49:45 +0900

    make private method

--- src/main/java/org/docksidestage/app/web/member/MemberEditAction.java
+++ src/main/java/org/docksidestage/app/web/member/MemberEditAction.java
@@ -68,11 +68,7 @@ public class MemberEditAction extends HarborBaseAction {
         validate(form, messages -> {}, () -> {
             return asHtml(path_Member_MemberEditHtml);
         });
-        Member member = new Member();
-        member.setMemberId(form.memberId);
-        member.setMemberStatusCode_Withdrawal();
-        member.setVersionNo(form.versionNo);
-        memberBhv.update(member);
+        updateMemberToWithdrawal(form);
         return redirect(MemberListAction.class);
     }
 
@@ -109,6 +105,14 @@ public class MemberEditAction extends HarborBaseAction {
         member.setVersionNo(form.versionNo);
         memberBhv.update(member);
         return member;
+    }
+
+    private void updateMemberToWithdrawal(MemberEditForm form) {
+        Member member = new Member();
+        member.setMemberId(form.memberId);
+        member.setMemberStatusCode_Withdrawal();
+        member.setVersionNo(form.versionNo);
+        memberBhv.update(member);
     }
 
     // ===================================================================================

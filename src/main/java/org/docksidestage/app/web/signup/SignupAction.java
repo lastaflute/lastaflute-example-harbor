9cb0d4eae05eb8e7261aea49e1d2caa28e3ac9a5
jflute <dbflute@gmail.com>
2017-04-16 02:26:05 +0900

    improve simple_for_example

--- src/main/java/org/docksidestage/app/web/signup/SignupAction.java
+++ src/main/java/org/docksidestage/app/web/signup/SignupAction.java
@@ -18,7 +18,6 @@ import org.docksidestage.mylasta.action.HarborMessages;
 import org.docksidestage.mylasta.direction.HarborConfig;
 import org.docksidestage.mylasta.mail.member.WelcomeMemberPostcard;
 import org.lastaflute.core.mail.Postbox;
-import org.lastaflute.core.security.PrimaryCipher;
 import org.lastaflute.core.util.LaStringUtil;
 import org.lastaflute.web.Execute;
 import org.lastaflute.web.login.AllowAnyoneAccess;
@@ -41,8 +40,6 @@ public class SignupAction extends HarborBaseAction {
     // ===================================================================================
     //                                                                           Attribute
     //                                                                           =========
-    @Resource
-    private PrimaryCipher primaryCipher;
     @Resource
     private Postbox postbox;
     @Resource
@@ -94,7 +91,7 @@ public class SignupAction extends HarborBaseAction {
     }
 
     private String saveSignupToken() {
-        String token = primaryCipher.encrypt(String.valueOf(new Random().nextInt())); // #simple_for_example
+        String token = Integer.toHexString(new Random().nextInt()); // #simple_for_example
         sessionManager.setAttribute(SIGNUP_TOKEN_KEY, token); // #simple_for_example
         return token;
     }

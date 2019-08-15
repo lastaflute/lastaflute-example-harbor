b4ec97b9f8f4abfa3725aa9a70c9af04e15f6814
jflute <dbflute@gmail.com>
2019-08-15 18:52:08 +0900

    fix #df:checkUser([argument])#

--- etc/startup/mysql-replace-schema-00-system.sql
+++ etc/startup/mysql-replace-schema-00-system.sql
@@ -4,11 +4,11 @@
 create database /*$dfprop.mainCatalog*/;
 
 -- #df:reviveUser()#
--- #df:checkUser(mainUser, grant)#
+-- #df:checkUser(mainUser)#
 create user /*$dfprop.mainUser*/@localhost identified by '/*$dfprop.mainPassword*/';
 
 -- #df:reviveUser()#
--- #df:checkUser(mainUser, grant)#
+-- #df:checkUser(grant)#
 grant all privileges on /*$dfprop.mainCatalog*/.* to /*$dfprop.mainUser*/@localhost;
 
 flush privileges;
